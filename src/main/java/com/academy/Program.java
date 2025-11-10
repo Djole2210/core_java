de99f7c0a97a5d81dfd391f4857034dabf932859
Djole2210 <djordje.knezevic97@gmail.com>
2025-11-10 18:41:34 +0100

    Dodat je tekst u System.out.println

--- src/main/java/com/academy/Program.java
+++ src/main/java/com/academy/Program.java
@@ -2,6 +2,6 @@ package com.academy;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Vozdra raja iz IntelLIj");
+        System.out.println("Vozdra raja iz IntelLIj.Trenutno ucimo git");
     }
 }
